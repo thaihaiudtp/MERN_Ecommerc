68cfe0fa03db706d9c66da7c925c7f991eed4262
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2024-09-07 01:26:13 +0700

    update

--- backend/src/app/controller/productController.js
+++ backend/src/app/controller/productController.js
@@ -1,5 +1,5 @@
 const Product = require('../model/product')
-
+const slugify = require('slugify')
 class ProductController {
     async create(req, res){
         if(Object.keys(req.body).length === 0){
@@ -19,8 +19,30 @@ class ProductController {
             message: products
         })
     }
+    async update(req, res) {
+        const {pid} = req.params
+        if(req.body && req.body.title) req.body.slug = slugify(req.body.title)
+        const updateProduct = await Product.findByIdAndUpdate(pid, req.body, {new: true})
+        console.log(updateProduct)
+        return res.status(200).json({
+            success:  updateProduct? true: false,
+            message: updateProduct
+        })
+    }
     async delete(req, res){
-        
+        const {pid} = req.params
+        const deleteProduct = await Product.delete({_id: pid})
+        return res.status(200).json({
+            success:  deleteProduct? true: false,
+            message: "Thành công"
+        })
+    }
+    async getdeleted(req, res){
+        const products = await Product.find({deleted: true}).select("-deleted")
+        return res.status(200).json({
+            success: products ? true: false,
+            message: products
+        })
     }
 }
 module.exports = new ProductController
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -5,4 +5,7 @@ const route = express.Router()
 
 route.post('/create', verifyToken.verifyTokenAdmin, productController.create)
 route.get('/getall', productController.getall)
+route.get('/getdeleted',verifyToken.verifyTokenAdmin, productController.getdeleted)
+route.put('/update/:pid', verifyToken.verifyTokenAdmin, productController.update)
+route.delete('/delete/:pid', verifyToken.verifyTokenAdmin, productController.delete)
 module.exports = route
